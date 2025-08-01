f391d56c1b08c136d97ae3fcaf821f9e92a690dc
ojas235767 <72729219+ojas235767@users.noreply.github.com>
2025-08-01 11:10:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Calculator from './components/Calculator';
 import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Calculator />
+      <About />
       <Footer />
     </div>
   );
 }
 
 export default App;
-
